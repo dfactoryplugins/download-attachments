ed965b0718d5cfb8f673fb7c8018a755cff6eed7
psielicki <48321567+psielicki@users.noreply.github.com>
2020-12-18 16:46:15 +0100

    Small fix for dynatable

--- assets/jquery-dynatable/jquery.dynatable.js
+++ assets/jquery-dynatable/jquery.dynatable.js
@@ -1380,8 +1380,8 @@
       // only bind page handler to non-active and non-disabled page links
       var selector = '#dynatable-pagination-links-' + obj.element.id + ' a.' + pageLinkClass + ':not(.' + activePageClass + ',.' + disabledPageClass + ')';
       // kill any existing delegated-bindings so they don't stack up
-      $(document).undelegate(selector, 'click.dynatable');
-      $(document).delegate(selector, 'click.dynatable', function(e) {
+      $(document).off(selector, 'click.dynatable');
+      $(document).on(selector, 'click.dynatable', function(e) {
         $this = $(this);
         $this.closest(settings.inputs.paginationClass).find('.' + activePageClass).removeClass(activePageClass);
         $this.addClass(activePageClass);
